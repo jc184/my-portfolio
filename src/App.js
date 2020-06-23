e4bd231989522aff118049db969dcd9717fd2d27
James Chalmers <james.chalmers184@gmail.com>
2020-06-23 16:30:49 +0100

    webpack and test image added

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ import {
 } from 'bootstrap-4-react';
 
 const thumbnailSrc = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22508%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20508%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16544932167%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16544932167%22%3E%3Crect%20width%3D%22508%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22169.7578125%22%20y%3D%22123.9%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
+export const contosothumbnail = require('./ContosoUniversityThumbnail.png');
 
 const AlbumCard = (props) => {
   return (
@@ -45,7 +46,7 @@ const AlbumCard1 = (props) => {
   return (
     <Col md="4">
       <Card mb="4" shadow="sm">
-        <Card.Image top src={thumbnailSrc} />
+        <Card.Image top src={contosothumbnail}/>
         <Card.Body>
           <Card.Text>Contoso University is an ASP.NET Core MVC web application, created by following this <BA href="https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/?view=aspnetcore-3.1" text="black">Microsoft tutorial</BA> It uses MVC and EF Core to link to Sql Server database hosted on Azure. The app is also hosted on Azure and was published by using Visual Studio 2019's 'publish to Azure' wizard.</Card.Text>
           <BDiv display="flex" justifyContent="between" alignItems="center">
@@ -75,10 +76,10 @@ const AlbumHeader = (props) => {
               <BH4 text="white">Contact</BH4>
               <List unstyled>
                 <List.Item><BA href="#" text="white">Email me</BA></List.Item>
-                <List.Item><BA href="https://www.linkedin.com/in/james-chalmers-a47a85a6/" text="white">LinkedIn</BA></List.Item>
-                <List.Item><BA href="https://stackoverflow.com/users/story/5830202" text="white">Stack Overflow</BA></List.Item>
-                <List.Item><BA href="https://github.com/jc184" text="white">Github</BA></List.Item>
-                <List.Item><BA href="https://happy-jepsen-102e57.netlify.app/blog/" text="white">My Blog</BA></List.Item>
+                <List.Item><BA href="https://www.linkedin.com/in/james-chalmers-a47a85a6/" text="white" target="_blank">LinkedIn</BA></List.Item>
+                <List.Item><BA href="https://stackoverflow.com/users/story/5830202" text="white" target="_blank">Stack Overflow</BA></List.Item>
+                <List.Item><BA href="https://github.com/jc184" text="white" target="_blank">Github</BA></List.Item>
+                <List.Item><BA href="https://happy-jepsen-102e57.netlify.app/blog/" text="white" target="_blank">My Blog</BA></List.Item>
               </List>
             </Col>
           </Row>
