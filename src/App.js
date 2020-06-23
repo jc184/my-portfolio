89b292dbadfe00632fd697ef507c79630709ab0c
James Chalmers <james.chalmers184@gmail.com>
2020-06-23 08:31:56 +0100

    content updated #9

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import {
   BSmall,
   BFooter
 } from 'bootstrap-4-react';
+import {Link} from 'react-router-dom'
 
 const thumbnailSrc = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22508%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20508%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16544932167%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16544932167%22%3E%3Crect%20width%3D%22508%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22169.7578125%22%20y%3D%22123.9%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
 
@@ -50,7 +51,9 @@ const AlbumCard1 = (props) => {
           <Card.Text>Contoso University is an ASP.NET Core MVC web application, created by following this <BA href="https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/?view=aspnetcore-3.1" text="black">Microsoft tutorial</BA> It uses MVC and EF Core to link to Sql Server database hosted on Azure. THe app is also hosted on Azure and was published by using Visual Studio 2019's 'publish to Azure' wizard.</Card.Text>
           <BDiv display="flex" justifyContent="between" alignItems="center">
             <ButtonGroup>
-              <Button secondary outline sm href="https://contosouniversity20200621180934.azurewebsites.net/">View</Button>
+              <Link to='https://contosouniversity20200621180934.azurewebsites.net/'>
+                <Button secondary outline sm>View</Button>
+              </Link>
               <Button secondary outline sm>Source</Button>
             </ButtonGroup>
             <BSmall text="muted">9 mins</BSmall>
