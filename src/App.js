fbd13344f8af420f391c2288098f9410ed7ad35b
James Chalmers <james.chalmers184@gmail.com>
2020-06-24 00:48:06 +0100

    rails card #3 added

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ import {
 const thumbnailSrc = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22508%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20508%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16544932167%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16544932167%22%3E%3Crect%20width%3D%22508%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22169.7578125%22%20y%3D%22123.9%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
 export const contosothumbnail = require('./ContosoUniversityThumbnail.png');
 export const listofingredientsthumbnail = require('./LIstOfIngredients.png');
+export const todoapplicationthumbnail = require('./ToDoApplication.png');
 
 const AlbumCard = (props) => {
   return (
@@ -73,11 +74,35 @@ const AlbumCard2 = (props) => {
           <h4>List of Ingredients</h4>
           <Card.Text>List of Ingredients is a Ruby on Rails API web application that uses a React frontend and stores its data in an ActiveAdmin CMS. 
             It was created by following this <BA href="https://blog.heroku.com/a-rock-solid-modern-web-stack" text="black">Heroku Rails tutorial </BA> 
-            It uses Heroku deplyment to host the app with a PostgreSQL database, also on Heroku. The app can be administered by appending the string 
+            It uses Heroku deployment to host the app with a PostgreSQL database, also on Heroku. The app can be administered by appending the string 
             '/admin' to the url, and carrying out Create / Read / Update / Delete actions on the ActiveAdmin CMS.</Card.Text>
           <BDiv display="flex" justifyContent="between" alignItems="center">
             <ButtonGroup>
               <Button type="button" secondary outline sm onClick={()=> window.open("https://peaceful-wave-81061.herokuapp.com/", "_blank")}>View</Button>
+              <Button secondary outline sm >Source</Button>
+            </ButtonGroup>
+            <BSmall text="muted">9 mins</BSmall>
+          </BDiv>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+}
+
+const AlbumCard3 = (props) => {
+  return (
+    <Col md="4">
+      <Card mb="4" shadow="sm">
+        <Card.Image top src={todoapplicationthumbnail} onClick={()=> window.open("https://guarded-inlet-64438.herokuapp.com/", "_blank")}/>
+        <Card.Body>
+          <h4>ToDo Application</h4>
+          <Card.Text>The ToDo Application is a Ruby on Rails API web application that also uses a React frontend and stores its data in a PostgreSQL database. 
+            It was created by following this <BA href="https://stevepolito.design/blog/rails-react-tutorial/" text="black">Rails tutorial. </BA> 
+            It uses Heroku deployment to host the app with a PostgreSQL database, also on Heroku. The app features a secure login facility, and it allows ToDos
+            to be added, amended or deleted.</Card.Text>
+          <BDiv display="flex" justifyContent="between" alignItems="center">
+            <ButtonGroup>
+              <Button type="button" secondary outline sm onClick={()=> window.open("https://guarded-inlet-64438.herokuapp.com/", "_blank")}>View</Button>
               <Button secondary outline sm >Source</Button>
             </ButtonGroup>
             <BSmall text="muted">9 mins</BSmall>
@@ -168,7 +193,7 @@ class App extends Component {
               <Row>
                 <AlbumCard1 />
                 <AlbumCard2 />
-                <AlbumCard />
+                <AlbumCard3 />
                 <AlbumCard />
                 <AlbumCard />
                 <AlbumCard />
